fc7b337d6e0ce7589543a87d2043048134907eff
dylanmcd <37030213+mcdonalddylan@users.noreply.github.com>
2020-10-16 12:20:13 -0400

    Update DirectServlet.java

--- HelloTomcat/src/main/java/com/web/servlet/DirectServlet.java
+++ HelloTomcat/src/main/java/com/web/servlet/DirectServlet.java
@@ -10,13 +10,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.catalina.connector.Connector;
-
 @WebServlet(name = "direct", urlPatterns = {"/direct"}, loadOnStartup = 0)
 public class DirectServlet extends HttpServlet {
 
-	Connector con = new Connector();
-	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setContentType("text/html");
